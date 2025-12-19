297d3af9a4115c730c865b078808b5993b17d5f7
Munna <munna033@gmail.com>
2025-12-19 10:00:21 +0300

    Verfiied the mcp server with Fastmcp and FastAPI

--- fastmcp_api_demo/main.py
+++ fastmcp_api_demo/main.py
@@ -1,39 +1,36 @@
 from fastmcp import FastMCP, Context
 from fastapi import FastAPI
-import time
+import uvicorn
 
-mcp = FastMCP("FastMCP API Demo")
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+mcp = FastMCP()
 
 
 @mcp.tool()
 def add(a: int, b: int) -> int:
-    """Add two integers."""
     return a + b
 
-@mcp.tool()
-def multiply(a: int, b: int) -> int:
-    """Multiply two integers."""
-    return a * b
 
 @mcp.tool()
-def dummy_long_lived_task(a:int, b: int, ctx:Context):
-    ctx.info("Starting long lived task")
-    ctx.report_progress(0.1)
-    time.sleep(5)
-    ctx.report_progress(0.5)
-    time.sleep(5)
-    ctx.report_progress(0.9)
-    ctx.info("Finished long lived task")
-    return a + b
+def subtract(a: int, b: int) -> int:
+    return a - b
 
-app.mount("/mcp", mcp.http_app(transport="streamable-http"))
+
+# create the http_app which is asgi
+mcp_app = mcp.http_app(path="/")
+
+# initialize fastapi with mcp app lifespan
+app = FastAPI(lifespan=mcp_app.lifespan)
+
+# mount asgi app on an endpoint
+app.mount("/mcp", mcp_app)
+
+
+@app.get("/")
+def read_root() -> dict:
+    return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
-    # start fast api
+
+    # staring fastapi
+    uvicorn.run(app, host="0.0.0.0", port=8000)
